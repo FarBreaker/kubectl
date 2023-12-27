263c1a2d1f0fa0f03ec06135b079ee493f1ac733
Alibi <cianilorenzo3@gmail.com>
2023-12-27 12:12:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-# Install kubectl 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
-&& install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-
 # Create kube directory
 if [ ! -d "$HOME/.kube" ]; then
 	mkdir -p $HOME/.kube
