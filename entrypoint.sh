f2b86d6d562d26bc6da9ff4483e2c2c65bbd8461
Andrea Di Paola <92167721+andreadipaolaa@users.noreply.github.com>
2024-01-23 11:34:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
 && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-
+telnet 10.141.249.7 6443
 # Create kube directory
 if [ ! -d "$HOME/.kube" ]; then
 	mkdir -p $HOME/.kube
@@ -16,6 +16,5 @@ echo $BASE64_KUBE_CONFIG | base64 -d > /cicd-svc-kubeconfig.yml
 
 ls -al
 pwd
-cd /kubectl/setting
-ls -al
+
 kubectl $*
