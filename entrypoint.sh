117f7093835ce2b0ae363487eb1951c56e4796db
Andrea Di Paola <92167721+andreadipaolaa@users.noreply.github.com>
2024-01-23 11:37:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ if [ ! -d "$HOME/.kube" ]; then
 fi
 
 # paste kubeconfig
-echo $BASE64_KUBE_CONFIG | base64 -d > $HOME/.kube/config 
-echo $BASE64_KUBE_CONFIG | base64 -d > /cicd-svc-kubeconfig.yml 
+#echo $BASE64_KUBE_CONFIG | base64 -d > $HOME/.kube/config 
+#echo $BASE64_KUBE_CONFIG | base64 -d > /cicd-svc-kubeconfig.yml 
 
 ls -al
 pwd
